0c25a4dc080739d42b25294eda379eff709ae8f8
Dan Pitic <dpitic@gmail.com>
2018-06-10 20:24:28 +0800

    Added Oni config files.

---
+++ oni/config.tsx
@@ -0,0 +1,42 @@
+
+import * as React from "react"
+import * as Oni from "oni-api"
+
+export const activate = (oni: Oni.Plugin.Api) => {
+    console.log("config activated")
+
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    // oni.input.unbind("<c-p>")
+
+}
+
+export const deactivate = (oni: Oni.Plugin.Api) => {
+    console.log("config deactivated")
+}
+
+export const configuration = {
+    //add custom config here, such as
+
+    "ui.colorscheme": "nord",
+
+    //"oni.useDefaultConfig": true,
+    //"oni.bookmarks": ["~/Documents"],
+    //"oni.loadInitVim": false,
+    //"editor.fontSize": "12px",
+    //"editor.fontFamily": "Monaco",
+    "editor.quickOpen.execCommand": "ls",
+    "oni.loadInitVim": "/home/dpitic/.config/oni/oni-init.vim",
+    "editor.cursorLineOpacity": 0.5,
+
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+}
---
+++ oni/oni-init.vim
@@ -0,0 +1,4 @@
+" Show right margin at column 80
+set cc=80
+" Set default assembly file syntax to nasm
+au BufRead,BufNewFile *.asm set filetype=nasm
